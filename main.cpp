f31113205aca13889fe944001585637bc7383ae9
Owliie <sladurcheto5232@abv.bg>
2021-01-10 13:06:09 +0200

    Added coloring for console prints Simplified convert function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <string>
+
+#define NC "\e[0m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define CYN "\e[0;36m"
+
 using namespace std;
 
 int const MIN_SECONDS = 0;
@@ -10,9 +16,7 @@ string convertToMinutes(int seconds){
 	int minutes = seconds/SECONDS_IN_MINUTE;
 	seconds %= SECONDS_IN_MINUTE;
 
-	string time = to_string(minutes);
-	time.push_back(':');
-	time.append(to_string(seconds));
+	string time = to_string(minutes) + ':' + to_string(seconds);
 
 	return time;
 }
@@ -23,12 +27,12 @@ int main(int argc, char const *argv[])
 
 	do
 	{
-		cout << "Enter seconds: ";
+		cout << CYN << "Enter seconds: ";
 		cin >> seconds;
 
 		if (seconds < 0 || seconds > 9999)
 		{
-			cout << "Invalid seconds format!" << endl;
+			cout << RED << "Invalid seconds format!" << endl << endl;
 		}
 	} while (seconds < 0 || seconds > 9999);
 
