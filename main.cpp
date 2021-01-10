0749a94dcef1786634bee5143f1bf44d4682d1f3
Owliie <sladurcheto5232@abv.bg>
2021-01-10 13:58:04 +0200

    Added print to console with interval 1s Added checks for minutes and seconds value Added beep sound when finished

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iomanip>
 
 #define NC "\e[0m"
 #define RED "\e[0;31m"
@@ -12,13 +15,50 @@ int const MIN_SECONDS = 0;
 int const MAX_SECONDS = 9999;
 int const SECONDS_IN_MINUTE = 60;
 
-string convertToMinutes(int seconds){
-	int minutes = seconds/SECONDS_IN_MINUTE;
+int *convertToMinutes(int seconds)
+{
+	int minutes = seconds / SECONDS_IN_MINUTE;
 	seconds %= SECONDS_IN_MINUTE;
 
-	string time = to_string(minutes) + ':' + to_string(seconds);
+	int *time = new int[2]{minutes, seconds};
 
 	return time;
+}
+
+void display_clock();
+
+void beep_sound()
+{
+	cout << '\7';
+}
+
+void start_timer(int *time)
+{
+	int minutes = time[0];
+	int seconds = time[1];
+
+	while (true)
+	{
+		system("clear");
+		cout << NC << setfill('0')
+			 << setw(2) << minutes << ':' << setw(2) << seconds << endl;
+		sleep(1);
+
+		if (minutes == MIN_SECONDS && seconds == MIN_SECONDS)
+		{
+			break;
+		}
+
+		seconds--;
+
+		if (seconds < MIN_SECONDS)
+		{
+			minutes--;
+			seconds = SECONDS_IN_MINUTE - 1;
+		}
+	}
+
+	beep_sound();
 }
 
 int main(int argc, char const *argv[])
@@ -32,11 +72,13 @@ int main(int argc, char const *argv[])
 
 		if (seconds < 0 || seconds > 9999)
 		{
-			cout << RED << "Invalid seconds format!" << endl << endl;
+			cout << RED << "Invalid seconds format!" << endl
+				 << endl;
 		}
 	} while (seconds < 0 || seconds > 9999);
 
-	cout << convertToMinutes(seconds) << endl;
+	int *time = convertToMinutes(seconds);
+	start_timer(time);
 
 	return 0;
 }
